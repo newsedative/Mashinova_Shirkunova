ec41ba6a6a2842c6370f3ba00dde0c4322ad91e2
newsedative <knowslana@gmail.com>
2022-04-26 21:07:18 +0300

    1

--- for_db.py
+++ for_db.py
@@ -5,16 +5,17 @@ from data.users import User
 class BotDB:
     def __init__(self):
         db_session.global_init("db/user_data.db")
+        self.user = User()
+        self.db_sess = db_session.create_session()
 
-    def check(self):
-
+    def check(self, id_u):
+        q = self.db_sess.query(User).filter(User.user_id == id_u)
+        return self.db_sess.query(q.exists()).scalar()
 
     def add_us(self, id_u):
-        user = User()
-        user.user_id = id_u
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
+        self.user.user_id = id_u
+        self.db_sess.add(self.user)
+        self.db_sess.commit()
 
 
 bot_db = BotDB()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from for_db import BotDB
 
 
@@ -7,12 +8,35 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
-def forward_message(message):
-    bot.send_message(message.from_user.id, "Привет, я бот,"
-                                           "ТЕКСТ ПРИВЕТСТВИЯ")
+def start(message):
+    bot.send_message(message.from_user.id, "Привет, я бот, который сохранит твоё ментальное здоровье! ✨"
+                                           "Здесь ты найдёшь: аффирмации, дыхательную гимнастику, "
+                                           "звуки природы и музыку для медитации."
+                                           "")
     db_cr = BotDB()
-    db_cr.add_us(message.from_user.id)
-    bot.send_message(message.from_user.id, "Ты авторизован")
+    user_id = message.from_user.id
+    if not db_cr.check(user_id):
+        db_cr.add_us(user_id)
+        bot.send_message(message.from_user.id, "регистрация выполнена")
+    else:
+        bot.send_message(message.from_user.id, "вход выполнен")
+
+
+@bot.message_handler(commands=['what_is'])
+def what(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    medit = types.KeyboardButton('Что такое медитация')
+    affirm = types.KeyboardButton('Что такое аффирмация')
+    markup.add(medit, affirm)
+    bot.send_message(message.from_user.id, "вот ответ", reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if message.text == 'Что такое медитация':
+        bot.send_message(message.from_user.id, text='медитация это')
+    else:
+        bot.send_message(message.from_user.id, text='аффирма это')
 
 
 bot.polling(none_stop=True, interval=0)
