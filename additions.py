484f8c17f5f127e6e86b01b87421b4208294108d
newsedative <knowslana@gmail.com>
2022-05-03 21:43:56 +0300

    дополнения

---
+++ additions.py
@@ -0,0 +1,10 @@
+import random
+
+
+def add_sticker():
+    sticker_id = ['CAACAgIAAxkBAAEEm9tib5b2PBR8XrCgGzefs3hvhDbXjgACPRwAAlt3uEpBE2TisTtDdiQE',
+                  'CAACAgIAAxkBAAEEm-Fib6CQ8a_iigbcs3RNAfqH6yZmlwACGxMAAifBcUnRvYBFTxWL5SQE',
+                  'CAACAgIAAxkBAAEEm-dib6Gjjx07RZoEjZMXdZduZzrCewACAQEAAladvQoivp8OuMLmNCQE',
+                  'CAACAgIAAxkBAAEEnkhicXdJvHnXYDqWv1ATMFI6692uSQACbgADwDZPE22H7UqzeJmXJAQ',
+                  'CAACAgIAAxkBAAEEnlRicXfJgJp1RNNWl3VbHl5mrM0vwAACfk8CAAFji0YMX7n3Hno1B08kBA']
+    return random.choice(sticker_id)
